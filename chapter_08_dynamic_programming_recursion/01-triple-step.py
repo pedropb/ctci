447740e61fde154b58c917dac5bace88e31e6798
Pedro Baracho <pedropbaracho@gmail.com>
2020-09-07 17:36:57 -0700

    Add problem definition to triple step and robot grid

--- chapter_08_dynamic_programming_recursion/01-triple-step.py
+++ chapter_08_dynamic_programming_recursion/01-triple-step.py
@@ -1,3 +1,7 @@
+"""
+Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+"""
+
 def count_ways(number_of_steps, memo) -> int:
   if number_of_steps == 0:
     return 1
